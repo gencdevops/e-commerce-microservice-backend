9d2d33b89e26859d053a80347098c36b307e0b33
Burhan Taşkesen <taskesenburhanoff@gmail.com>
2023-04-08 21:04:32 +0300

    [Burhan TAŞKESEN] - product controller

---
+++ product-service/src/main/java/com/fmss/productservice/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.fmss.productservice.controller;
+
+import com.fmss.productservice.model.dto.ProductResponseDto;
+import com.fmss.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+}
