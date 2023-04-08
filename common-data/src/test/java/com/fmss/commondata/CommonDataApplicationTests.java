af2419df38641c498d882514cbf51501a02d53a5
Oguzcan BICER <oguzcanbicer96@gmail.com>
2023-04-08 15:44:57 +0300

    [Oguzcan BICER]-ELK ve common-data

---
+++ common-data/src/test/java/com/fmss/commondata/CommonDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmss.commondata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
