f27a6d929d43fe53a203a467737ed18c648e00f3
Burhan TaşkesenBurhan Taşkesen <taskesenburhanoff@gmail.com>
2023-04-08 19:06:52 +0300

    [Burhan TAŞKESEN] - apigateway user service

---
+++ api-gateway/src/main/java/com/fmss/service/UserService.java
@@ -0,0 +1,24 @@
+package com.fmss.service;
+
+import com.fmss.commondata.model.LoginRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final WebClient userServiceWebClient;
+
+    public Mono<ResponseEntity<String>> login(LoginRequestDto loginRequestDto){
+        return userServiceWebClient
+                .post()
+                .uri("/login")
+                .bodyValue(loginRequestDto)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(response -> ResponseEntity.ok().header("token", response).body("Success"));
+    }
+}
