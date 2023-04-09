fb979f17f4e08c1f4d570a0e37e02d8b9fef6b90
Burhan Taşkesen <taskesenburhanoff@gmail.com>
2023-04-09 18:13:04 +0300

    [Burhan TAŞKESEN] - redis config for product microservice

--- product-service/src/main/java/com/fmss/productservice/config/RedisConfig.java
+++ product-service/src/main/java/com/fmss/productservice/config/RedisConfig.java
@@ -1,17 +1,28 @@
 package com.fmss.productservice.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fmss.productservice.model.dto.ProductRequestDto;
 import com.fmss.productservice.model.dto.ProductResponseDto;
 import com.fmss.productservice.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import redis.clients.jedis.JedisPoolConfig;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -23,9 +34,36 @@ public class RedisConfig {
     }
 
     @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+                //.disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .transactionAware()
+                .build();
+    }
+
+    @Bean
+    public GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
+
+    @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(jackson2JsonRedisSerializer());
+        template.setHashValueSerializer(RedisSerializer.json());
+        template.setHashKeySerializer(RedisSerializer.string());
         return template;
     }
 
