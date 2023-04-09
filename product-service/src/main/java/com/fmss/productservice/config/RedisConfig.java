79367ddd25fbc06d920a82d267c34ff8370c89fe
Burhan Taşkesen <taskesenburhanoff@gmail.com>
2023-04-09 18:49:08 +0300

    [Burhan TAŞKESEN] - update redis config for product microservice

--- product-service/src/main/java/com/fmss/productservice/config/RedisConfig.java
+++ product-service/src/main/java/com/fmss/productservice/config/RedisConfig.java
@@ -1,11 +1,6 @@
 package com.fmss.productservice.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fmss.productservice.model.dto.ProductRequestDto;
-import com.fmss.productservice.model.dto.ProductResponseDto;
 import com.fmss.productservice.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cache.CacheManager;
@@ -17,13 +12,11 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.math.BigDecimal;
 import java.time.Duration;
-import java.util.List;
 
 @Configuration
 public class RedisConfig {
@@ -48,20 +41,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-
-        return new GenericJackson2JsonRedisSerializer(objectMapper);
-    }
-
-    @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         template.setKeySerializer(RedisSerializer.string());
-        template.setValueSerializer(jackson2JsonRedisSerializer());
+        template.setValueSerializer(RedisSerializer.json());
         template.setHashValueSerializer(RedisSerializer.json());
         template.setHashKeySerializer(RedisSerializer.string());
         return template;
@@ -75,7 +59,7 @@ public class RedisConfig {
             productService.createProduct(new ProductRequestDto("test3", BigDecimal.ZERO, true));
 
             productService.getAllProducts();
-            var allProducts = (List<ProductResponseDto>) redisTemplate().opsForValue().get("allProducts");
+            //var allProducts = (List<ProductResponseDto>) redisTemplate().opsForValue().get("allProducts");
 
             System.out.println();
         };
