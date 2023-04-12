43eb7657ea3b81c7c43c5d21df3a8c330d3050d7
Burhan Taşkesen <taskesenburhanoff@gmail.com>
2023-04-12 13:11:46 +0300

    [Burhan TAŞKESEN] - basket controller adding crud operations

--- basket-service/src/main/java/com/fmss/basketservice/controller/BasketController.java
+++ basket-service/src/main/java/com/fmss/basketservice/controller/BasketController.java
@@ -1,7 +1,12 @@
 package com.fmss.basketservice.controller;
 
+import com.fmss.basketservice.model.dto.BasketItemRequestDto;
+import com.fmss.basketservice.model.dto.BasketItemResponseDto;
+import com.fmss.basketservice.model.dto.BasketResponseDto;
 import com.fmss.basketservice.service.BasketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,39 +16,36 @@ public class BasketController {
 
     private final BasketService basketService;
 
-
-    @PostMapping
-    public void createBasket(String userId) {
-        basketService.createBasket(userId);
+    @GetMapping("/basket/{userId}") //endpoint doğru mu ?
+    public ResponseEntity<BasketResponseDto> getBasketByUserId(@PathVariable String userId) {
+        return ResponseEntity.ok(basketService.getBasketByUserId(userId));
     }
 
-    @GetMapping("/userid/{userId}")
-    public void getBasketByUserId(@PathVariable String userId) {
-
+    @GetMapping("/basket/{basketId}")
+    public ResponseEntity<BasketResponseDto> getBasketByBasketId(@PathVariable String basketId) {
+        return ResponseEntity.ok(basketService.getBasketByBasketId(basketId));
     }
 
-    @GetMapping("/basketId/{basketId}")
-    public void getBasketByBasketId(@PathVariable String basketId) {
-
+    @PutMapping("/basket/disable/{basketId}")
+    public void disableBasket(@PathVariable String basketId) {
+        basketService.disableBasket(basketId);
     }
-//
-//    @PutMapping
-//    public void disableBasket() {
-//
-//    }
-//
-//    @DeleteMapping
-//    public void deleteBasket() {
-//
-//    }
-//
-//    @PostMapping
-//    public void addBasketItemToBasket(BasketItem basketItem) {
-//
-//    }
-//
-//    @PostMapping
-//    public void deleteBasketItemOnBasket(BasketItem basketItem) {
-//
-//    }
+
+    @DeleteMapping("/{basketId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBasket(@PathVariable String basketId) {
+        basketService.deleteBasket(basketId);
+    }
+
+    @PostMapping("/basket-item")
+    public ResponseEntity<BasketItemResponseDto> addBasketItemToBasket(@RequestBody BasketItemRequestDto basketItemRequestDto) {
+        return ResponseEntity.ok(basketService.addBasketItemToBasket(basketItemRequestDto));
+    }
+
+
+    @DeleteMapping("/basket-item/{basketItemId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBasketItemOnBasket(String basketItemId) {
+        basketService.deleteBasketItemFromBasket(basketItemId);
+    }
 }
