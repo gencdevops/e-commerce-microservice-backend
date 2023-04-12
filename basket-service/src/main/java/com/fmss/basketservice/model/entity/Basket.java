a21ed36b99232c4d197cdf672b1e1e462efe34f1
Oguzcan BICER <oguzcanbicer96@gmail.com>
2023-04-12 22:14:01 +0300

    [Oguzcan BICER]-common

--- basket-service/src/main/java/com/fmss/basketservice/model/entity/Basket.java
+++ basket-service/src/main/java/com/fmss/basketservice/model/entity/Basket.java
@@ -7,13 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
-@SuperBuilder
 @Getter
 @Setter
 @AllArgsConstructor
