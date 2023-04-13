31c244baa4c36d5ac53d012e16e02b11180962e9
Oguzcan BICER <oguzcanbicer96@gmail.com>
2023-04-14 02:09:51 +0300

    [Oguzcan BICER]-product unit test

--- product-service/src/main/java/com/fmss/productservice/mapper/ProductMapper.java
+++ product-service/src/main/java/com/fmss/productservice/mapper/ProductMapper.java
@@ -5,8 +5,9 @@ import com.fmss.productservice.model.dto.ProductRequestDto;
 import com.fmss.productservice.model.dto.ProductResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(implementationName = "PaymentMapperImpl", componentModel = "spring")
+@Mapper(implementationName = "PaymentMapperImpl", componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapper {
 
     @Mapping(target = "image", source = "url")
